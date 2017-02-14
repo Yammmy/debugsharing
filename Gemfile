075018d79d6db42cc618e248b8a8ebfb7a8af92f
Yammy <yammyxing@foxmail.com>
2017-02-14 10:14:05 +0800

    install fog to manage aws

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem 'will_paginate-bootstrap'
 gem 'letter_opener', group: :development
 gem 'aasm'
 gem 'figaro'
+gem 'fog'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AKIAIHV4LQKNPRUTVC4Q"],
+
+
+      aws_secret_access_key: ENV["aCSqvV3CKhAXND1pdt93o22TzvSMXD9nHMiTLz97"],
+
+
+      region:                'ap-northeast-1'
+
+
+    }
+    config.fog_directory  = ENV["fullstacktry"] 
+
+
+
+  else
+    config.storage :file
+  end
+end

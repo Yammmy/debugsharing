b38a8366547cd4da446d94a8478f326ded00ccec
Yammy <yammyxing@foxmail.com>
2017-02-14 16:42:29 +0800

    micro adjustment

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts "Test account created."
 
 products =[
 
-					{title:"Tiered Slip dress)",
+					{title:"Tiered Slip dress",
           description: "Tiered asymmetrical ruffles add flounce to a midi-length dress perfect for twirling on the dance floor.",
           image_path: "#{Rails.root}/app/assets/images/dress1.jpg"},
 
@@ -73,17 +73,17 @@ products =[
 
 
 
-p1 = Product.create([title:products[0][:title],description:products[0][:description],quantity:200,price:300,image:open(products[0][:image_path])])
-p2 = Product.create([title:products[1][:title],description:products[1][:description],quantity:200,price:300,image:open(products[1][:image_path])])
-p3 = Product.create([title:products[2][:title],description:products[2][:description],quantity:200,price:300,image:open(products[2][:image_path])])
-p4 = Product.create([title:products[3][:title],description:products[3][:description],quantity:200,price:300,image:open(products[3][:image_path])])
-p5 = Product.create([title:products[4][:title],description:products[4][:description],quantity:200,price:300,image:open(products[4][:image_path])])
-p6 = Product.create([title:products[5][:title],description:products[5][:description],quantity:200,price:300,image:open(products[5][:image_path])])
-p7 = Product.create([title:products[6][:title],description:products[6][:description],quantity:200,price:300,image:open(products[6][:image_path])])
-p8 = Product.create([title:products[7][:title],description:products[7][:description],quantity:200,price:300,image:open(products[7][:image_path])])
-p9 = Product.create([title:products[8][:title],description:products[8][:description],quantity:200,price:300,image:open(products[8][:image_path])])
-p10 = Product.create([title:products[9][:title],description:products[9][:description],quantity:200,price:300,image:open(products[9][:image_path])])
-p11 = Product.create([title:products[10][:title],description:products[10][:description],quantity:200,price:300,image:open(products[10][:image_path])])
-p12 = Product.create([title:products[11][:title],description:products[11][:description],quantity:200,price:300,image:open(products[11][:image_path])])
+p1 = Product.create([title:products[0][:title],description:products[0][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[0][:image_path])])
+p2 = Product.create([title:products[1][:title],description:products[1][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[1][:image_path])])
+p3 = Product.create([title:products[2][:title],description:products[2][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[2][:image_path])])
+p4 = Product.create([title:products[3][:title],description:products[3][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[3][:image_path])])
+p5 = Product.create([title:products[4][:title],description:products[4][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[4][:image_path])])
+p6 = Product.create([title:products[5][:title],description:products[5][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[5][:image_path])])
+p7 = Product.create([title:products[6][:title],description:products[6][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[6][:image_path])])
+p8 = Product.create([title:products[7][:title],description:products[7][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[7][:image_path])])
+p9 = Product.create([title:products[8][:title],description:products[8][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[8][:image_path])])
+p10 = Product.create([title:products[9][:title],description:products[9][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[9][:image_path])])
+p11 = Product.create([title:products[10][:title],description:products[10][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[10][:image_path])])
+p12 = Product.create([title:products[11][:title],description:products[11][:description],quantity:rand(3..9) * 100 ,price:rand(1..9) * 100 + 99,image:open(products[11][:image_path])])
 
 puts "Successfully create 12 products info."

a9ceecaf9005958480d8e5b2a4eab429a57c93fe
Yammy <yammyxing@foxmail.com>
2017-02-14 11:14:37 +0800

    fix figaro problem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,17 +3,17 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AKIAIHV4LQKNPRUTVC4Q"],
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
 
-      aws_secret_access_key: ENV["aCSqvV3CKhAXND1pdt93o22TzvSMXD9nHMiTLz97"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
 
       region:                'ap-northeast-1'
 
 
     }
-    config.fog_directory  = ENV["fullstacktry"]
+    config.fog_directory  = ENV["AWS_BUCKET_NAME"]
     config.fog_public     = false                                            # optional, defaults to true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 

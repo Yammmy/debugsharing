23ff1d9b0a4025abd63906909157988753f7dc9b
Yammy <yammyxing@foxmail.com>
2017-02-14 22:13:36 +0800

    fix application

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = 'fog/aws'
+    config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
@@ -9,16 +9,11 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
 
-      region:                'ap-northeast-1'
+      region:                ENV["AWS_BUCKET_REGION"]
 
 
     }
     config.fog_directory  = ENV["AWS_BUCKET_NAME"]
-    config.fog_public     = false                                            # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-
-
-
 
   else
     config.storage :file

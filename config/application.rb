baa50ee4a32714015307d1e8e723777dc8767388
Yammy <yammyxing@foxmail.com>
2017-02-24 12:03:46 +0800

    using MemCachier with dalli

--- config/application.rb
+++ config/application.rb
@@ -15,3 +15,13 @@ module Supermall
 end
 
 Fog.credentials = { path_style: true }
+
+config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2,
+                     :down_retry_delay => 60
+                    }

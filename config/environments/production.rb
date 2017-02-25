5edd06e9dd8ac0c1f647c00bf2c0424572d6430d
Yammy <yammyxing@foxmail.com>
2017-02-25 18:24:43 +0800

    write production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,16 +99,19 @@ Rails.application.configure do
 
      }
 
+
+   config.cache_store = :dalli_store,
+                       (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                       {:username => ENV["MEMCACHIER_USERNAME"],
+                        :password => ENV["MEMCACHIER_PASSWORD"],
+                        :failover => true,
+                        :socket_timeout => 1.5,
+                        :socket_failure_delay => 0.2,
+                        :down_retry_delay => 60
+                       }
+
    if Rails.env.production?
-     config.cache_store = :dalli_store,
-                         (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                         {:username => ENV["MEMCACHIER_USERNAME"],
-                          :password => ENV["MEMCACHIER_PASSWORD"],
-                          :failover => true,
-                          :socket_timeout => 1.5,
-                          :socket_failure_delay => 0.2,
-                          :down_retry_delay => 60
-                         }
+     config.cache_store = :dalli_store
    else
      self.cache_store = :mem_cache_store
    end

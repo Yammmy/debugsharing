16dd50959841a29e4a6c0d70aec10c1534a6e9a8
Yammy <yammyxing@foxmail.com>
2017-03-18 01:36:59 +0800

    add index to order counting user_id and rerun rake assets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306155100) do
+ActiveRecord::Schema.define(version: 20170317172419) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170306155100) do
     t.string   "payment_method"
     t.string   "aasm_state",       default: "order_placed"
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|

7d4b5d8241566a81871338bd9097d6053bbc0977
Yammy <yammyxing@foxmail.com>
2017-03-22 00:43:49 +0800

    revise mistake in order_mailer.rb

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,27 +1,35 @@
 class OrderMailer < ApplicationMailer
-  before_action :find_order_and_user
 
   def notify_order_placed(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
     mail(to: @user.email, subject: "[Debugsharing] thank you for complete this order, this is your product lists #{order.token} ")
   end
 
   def apply_cancell(order)
-    mail(to: @user.email, subject: "[Debugsharing] User#{order.user.name} pulled a request to cancell order#{order.token}")
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: "yammy7chris@gmail.com", subject: "[Debugsharing] User#{order.user.name} pulled a request to cancell order#{order.token}")
   end
 
   def notify_ship(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
     mail(to: @user.email, subject: "[Debugsharing] Your order#{order.token} is shipping!")
   end
 
   def notify_cancell(order)
-    mail(to: @user.email, subject: "[Debugsharing] Your order#{order.token} is cancelled!")
-  end
-
-  private
-
-  def find_order_and_user
     @order = order
     @user = order.user
     @product_lists = @order.product_lists
+    
+    mail(to: @user.email, subject: "[Debugsharing] Your order#{order.token} is cancelled!")
   end
+
 end

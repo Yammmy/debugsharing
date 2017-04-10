4dfb51c251ba51fd12fd1f17df11f82cb1c21b13
Yammy <yammyxing@foxmail.com>
2017-04-10 23:35:14 +0800

    add google html check

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get '/about' => 'welcome#about'
+  get '/google13b157faa1635496' => 'welcome#google13b157faa1635496'
+
 
   resources :conversations, only: [:index, :show, :destroy] do
     member do

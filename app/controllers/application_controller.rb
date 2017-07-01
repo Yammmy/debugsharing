ad74ee28c9d355d7f606a6e9198da10171c8b5d4
Yammy <yammyxing@foxmail.com>
2017-07-01 11:59:17 +0800

    avoid error in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,14 +30,14 @@ class ApplicationController < ActionController::Base
     @current_cart ||= find_cart
   end
 
-  # rescue_from ActiveRecord::RecordNotFound do
-  #   flash[:warning] = 'Resource not found.'
-  #   redirect_back_or root_path
-  # end
+  rescue_from ActiveRecord::RecordNotFound do
+    flash[:warning] = 'Resource not found.'
+    redirect_back_or root_path
+  end
 
-  # def redirect_back_or(path)
-  #   redirect_to request.referer || path
-  # end
+  def redirect_back_or(path)
+    redirect_to request.referer || path
+  end
 
   private
 
